73f635aef4059eb8d0c4d1cb78bf0a81a34e3e6e
jothish741 <maharajothish741@gmail.com>
2024-05-12 11:12:37 +0530

    Final Commit

---
+++ mathServer/mathServer/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('areaofprism/',views.prismarea,name="areaofprism"),
+    path('',views.prismarea,name="areaofprismroot")
+]
---
+++ mathServer/myapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+def prismarea(request):
+    context={}
+    context['area'] = "0"
+    context['l'] = "0"
+    context['b'] = "0"
+    if request.method == 'POST':
+        print("POST method is used")
+        l = request.POST.get('length','0')
+        b = request.POST.get('breadth','0')
+        print('request=',request)
+        print('Length=',l)
+        print('Breadth=',b)
+        area = 2*(int(l) * int(b) + int(b) * int(l))
+        context['area'] = area
+        context['l'] = l
+        context['b'] = b
+        print('Area=',area)
+    return render(request,'myapp/math.html',context)
